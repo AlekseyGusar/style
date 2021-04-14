0cc450a5d5978b5fd69b16e971de2a739bd1dc6d
AlekseyGusar <aleksey.gusar.2@gmail.com>
2021-04-14 20:34:40 +0300

    add styles

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,7 +1,8 @@
 import React from 'react';
 
 import Button from '../button/button.js';
-
+import './app.css';
+    
 export default function App(){
-    return <div data-testid="element-app"><Button /></div>
+    return <div data-testid="element-app" className="wrapper-app"><Button /></div>
 }
--- src/components/button/button.js
+++ src/components/button/button.js
@@ -1,5 +1,13 @@
 import React from 'react';
 
-export default function Button(){
-    return <button data-testid="element-button"></button>;
+function Button() {
+    const wrapper = {
+        color: 'rgb(18, 255, 18)',
+        backgroundColor: 'transparent',
+        border: 0,
+        fontFamily: 'Courier',
+        fontSize: '33pt'
+    };
+    return ( <button data-testid="element-button" style={wrapper}>Wake up Neo...</button>);
 }
+export default Button;
